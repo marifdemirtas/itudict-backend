17df67c9738725917834cfe1d5f542e2e8ee860c
Erce Can Bektüre <canerce99@gmail.com>
2022-12-19 00:06:43 +0300

    REFACTOR: refactored app module removed redundant endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://itudict:koWwJl32wwYK86jx@cluster0.opq8fwd.mongodb.net/?retryWrites=true&w=majority',
-    ),
-    ConfigModule.forRoot(), // load and parse .env file
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
