e6f9014b17ec8fdfc413d385cc176b78f4b139fd
Erce Can Bektüre <canerce99@gmail.com>
2022-12-19 01:29:32 +0300

    ADD: added auth module with passport-jwt strategy
    also added endpoints and auth dto

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,125 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import * as bcyrpt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UpdateUserDto } from 'src/user/dto/update-user.dto';
+import { User, UserDocument } from 'src/user/schemas/user.schema';
+import * as argon2 from 'argon2';
+import { AuthDto } from './dto/auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  hashData(data: string) {
+    return argon2.hash(data);
+  }
+
+  async signUp(createUserDto: CreateUserDto): Promise<any> {
+    // Check if user exists
+    //hash password here then send to user service
+    const userExists = await this.userService.findByEmail(createUserDto.email);
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    }
+
+    // hash password
+    const hash = bcyrpt.hashSync(createUserDto.password, 10);
+    const newUser = await this.userService.create({
+      ...createUserDto,
+      password: hash,
+    });
+    const tokens = await this.getTokens(newUser._id, newUser.email);
+    await this.updateRefreshToken(newUser._id, tokens.refreshToken);
+    return tokens;
+  }
+
+  async signIn(data: AuthDto) {
+    // Check if user exists
+    const user = await this.userService.findByEmail(data.email);
+    if (!user) throw new BadRequestException('User does not exist');
+    if (!bcyrpt.compareSync(data.password, user.password)) {
+      //const passwordMatches = await argon2.verify(user.password, data.password);
+      //if (!passwordMatches)
+      throw new BadRequestException('Password is incorrect');
+    }
+    const tokens = await this.getTokens(user._id, user.username);
+    await this.updateRefreshToken(user._id, tokens.refreshToken);
+    return tokens;
+  }
+
+  async updateRefreshToken(userId: string, refreshToken: string) {
+    const hashedRefreshToken = await this.hashData(refreshToken);
+    await this.userService.update(userId, {
+      refreshToken: hashedRefreshToken,
+    });
+  }
+
+  async getTokens(userId: string, username: string) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+        },
+        {
+          secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+          expiresIn: '15m',
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          username,
+        },
+        {
+          secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+          expiresIn: '7d',
+        },
+      ),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
+    if (user) {
+      if (bcyrpt.compareSync(password, user.password)) {
+        const { password, ...result } = user;
+        return result;
+      } else {
+        throw new Error('Incorrect password');
+      }
+    } else {
+      throw new Error('User not found');
+    }
+  }
+
+  /*
+  async validateRefreshToken(userId: string, refreshToken: string) {
+    const user = await this.userService.findById(userId);
+    if (!user) throw new BadRequestException('User does not exist');
+    const hashedRefreshToken = await this.hashData(refreshToken);
+    if (hashedRefreshToken !== user.refreshToken) {
+      throw new BadRequestException('Refresh token is invalid');
+    }
+    const tokens = await this.getTokens(user._id, user.username);
+    await this.updateRefreshToken(user._id, tokens.refreshToken);
+    return tokens;
+  }
+  */
+
+  async logout(userId: string) {
+    return this.userService.update(userId, { refreshToken: null });
+  }
+}
